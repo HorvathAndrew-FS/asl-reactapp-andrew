7d54f0ca7b6a6b0c89ec5f4a158803aafe0106a0
Andrew Horvath <skwerl@student.fullsail.edu>
2022-12-15 19:25:00 -0600

    finally added choices and seeded all seeder files

--- src/seeders/20221215233258-add-choices.js
+++ src/seeders/20221215233258-add-choices.js
@@ -22,33 +22,83 @@ module.exports = {
       updatedAt: new Date()
     },
     {
-      label: 'Question four?',
+      label: 'Choice four.',
       questionId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     // Question 2
     {
-      label: 'Choice one?',
+      label: 'Choice one.',
       questionId: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
-      label: 'Choice two?',
+      label: 'Choice two.',
       questionId: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
-      label: 'Choice three?',
+      label: 'Choice three.',
       questionId: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
-      label: 'Choice four?',
+      label: 'Choice four.',
       questionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    // Question 3
+    {
+      label: 'Choice one.',
+      questionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      label: 'Choice two.',
+      questionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      label: 'Choice three.',
+      questionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      label: 'Choice four.',
+      questionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    // Question 4
+    {
+      label: 'Choice one.',
+      questionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      label: 'Choice two.',
+      questionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      label: 'Choice three.',
+      questionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      label: 'Choice four.',
+      questionId: 4,
       createdAt: new Date(),
       updatedAt: new Date()
     },
@@ -58,8 +108,8 @@ down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Choices', {[Sequelize.Op.or]: [
       {questionId: 1}, 
       {questionId: 2},
-      // {questionId: 3},
-      // {questionId: 4},
+      {questionId: 3},
+      {questionId: 4},
     ]});
   }
 };
